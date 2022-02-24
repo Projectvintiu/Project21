8769f2aa1492ac16a282a55c3f78e8777cfc1ac4
didacflorensa <didacflorensacazorla@gmail.com>
2022-02-24 16:26:49 +0100

    Add project

---
+++ app/src/main/java/com/example/project21/Carta.java
@@ -0,0 +1,22 @@
+package com.example.project21;
+
+public class Carta {
+    private String palo;
+    private int numero;
+
+    public String getPalo() {
+        return palo;
+    }
+
+    public void setPalo(String palo) {
+        this.palo = palo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+}
---
+++ app/src/main/java/com/example/project21/Joc.java
@@ -0,0 +1,33 @@
+package com.example.project21;
+
+public class Joc {
+    private Jugador player;
+    private Jugador ia;
+    private Baraja baraja;
+
+    public Jugador getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Jugador player) {
+        this.player = player;
+    }
+
+    public Jugador getIa() {
+        return ia;
+    }
+
+    public void setIa(Jugador ia) {
+        this.ia = ia;
+    }
+
+    public Baraja getBaraja() {
+        return baraja;
+    }
+
+    public void setBaraja(Baraja baraja) {
+        this.baraja = baraja;
+    }
+
+
+}
